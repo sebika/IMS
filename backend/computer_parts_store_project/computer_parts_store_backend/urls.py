f62225e7041af3a4a3f63c5529a4df53e537b7ec
Cristian-Valeriu DIMITROV <cristian.dimitrov@stud.acs.upb.ro>
2024-01-18 09:49:35 +0000

    Update urls.py

--- backend/computer_parts_store_project/computer_parts_store_backend/urls.py
+++ backend/computer_parts_store_project/computer_parts_store_backend/urls.py
@@ -16,25 +16,33 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+
+from django.contrib import admin
+from django.urls import path, re_path, include
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
 schema_view = get_schema_view(
-   openapi.Info(
-      title="Snippets API",
-      default_version='v1',
-      description="Test description",
-    #   terms_of_service="https://www.google.com/policies/terms/",
-    #   contact=openapi.Contact(email="contact@snippets.local"),
-    #   license=openapi.License(name="BSD License"),
-   ),
-   public=True,
-   permission_classes=(permissions.AllowAny,),
+    openapi.Info(
+        title="Swagger DOC ISI",
+        default_version='v1',
+        description="No description",
+        terms_of_service="https://www.youtube.com/",
+        contact=openapi.Contact(email="nuincerca@gmail.com"),
+        license=openapi.License(name="Your License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
 )
 
 urlpatterns = [
-    path('api/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('computer_store/', include('computer_parts_store.urls')),
     path('admin/', admin.site.urls),
+    path('swagger(?P<format>\.json|\.yaml)', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc')
+
 ]
+
+
